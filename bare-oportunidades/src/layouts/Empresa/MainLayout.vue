b9161ff20a4cf9d0295b9ccfa4bb9abe6085db33
Caio Cesar <sys13@grupopmz.com.br>
2020-10-15 17:35:56 -0400

    criando card descritivo de vagas empresa

--- bare-oportunidades/src/layouts/Empresa/MainLayout.vue
+++ bare-oportunidades/src/layouts/Empresa/MainLayout.vue
@@ -9,6 +9,12 @@
     <main id="body-empresa">
       <div id="header-empresa">
         <h5>Lista de Vagas Oferecidas</h5>
+        <q-btn
+          style="background:white; color: #e65100;"
+          label="Nova Oportunidade"
+          outline
+          icon="work_outline"
+        />
       </div>
       <q-list bordered class="rounded-borders">
         <q-expansion-item
@@ -20,10 +26,54 @@
         >
           <q-card>
             <q-card-section>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
-              eius reprehenderit eos corrupti commodi magni quaerat ex numquam,
-              dolorum officiis modi facere maiores architecto suscipit iste
-              eveniet doloribus ullam aliquid.
+              <article id="body-vagas-empresa">
+                <section id="body-cont-vaga">
+                  <strong>Descrição da vaga</strong>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quidem, eius reprehenderit eos corrupti commodi magni
+                    quaerat ex numquam, dolorum officiis modi facere maiores
+                    architecto suscipit iste eveniet doloribus ullam aliquid.
+                  </p>
+
+                  <strong>Atributos</strong>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quidem, eius reprehenderit eos corrupti commodi magni
+                    quaerat ex numquam, dolorum officiis modi facere maiores
+                    architecto suscipit iste eveniet doloribus ullam aliquid.
+                  </p>
+                  <strong>Atributos</strong>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quidem, eius reprehenderit eos corrupti commodi magni
+                    quaerat ex numquam, dolorum officiis modi facere maiores
+                    architecto suscipit iste eveniet doloribus ullam aliquid.
+                  </p>
+                  <strong>Atributos</strong>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quidem, eius reprehenderit eos corrupti commodi magni
+                    quaerat ex numquam, dolorum officiis modi facere maiores
+                    architecto suscipit iste eveniet doloribus ullam aliquid.
+                  </p>
+                </section>
+                <section id="body-cont-candidatos">
+                  <q-btn
+                    style="background:white; color: #e65100; margin-right:2px"
+                    label="Avaliar Cadidatos"
+                    outline
+                    icon="person_add"
+                  />
+
+                  <q-btn
+                    style="background:white; color: #e65100;"
+                    label="Editar Oportunidade"
+                    outline
+                    icon="edit"
+                  />
+                </section>
+              </article>
             </q-card-section>
           </q-card>
         </q-expansion-item>
@@ -72,5 +122,12 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+#body-vagas-empresa {
+  padding: 1.5%;
+  display: flex;
+  align-items: flex-start;
+  justify-items: center;
+  flex-direction: column;
 }
 </style>
