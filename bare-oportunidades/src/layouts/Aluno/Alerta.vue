b24adbabd1174a18612c18e6c3067cdb1880ca40
Caio César <kaio.cesr@gmail.com>
2020-10-20 21:51:33 -0400

    criando carrosel de empresas componentes de aluno

---
+++ bare-oportunidades/src/layouts/Aluno/Alerta.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-banner dense inline-actions class="text-white  card" style="background:#e65100">
+      Seu Cadastro ainda não foi ativado! favor verificar com sua instituição
+      <template v-slot:action>
+        <q-btn flat color="white" label="Entendido" />
+      </template>
+   </q-banner>
+</template>
+
+<script>
+export default {
+  name: "Alerta",
+};
+</script>
+<style>
+.card{
+  margin-top:0.5%;
+}
+</style>
